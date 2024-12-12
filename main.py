5ff29b319510dc2297a175b60ad6526919a9d84a
AmcL <mcleodamare@gmail.com>
2024-12-12 14:17:29 -0500

    changed out the link

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get("http://selenium.dev")
-
-driver.quit()
+driver.get("https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D")
