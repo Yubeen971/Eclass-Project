ad1c462b7845ffec5655ffdb7483855e2d86e3e5
AmcL <mcleodamare@gmail.com>
2024-12-16 14:52:35 -0500

    Pull these specific columns from the CSV file

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
+import pandas as pd
 
-
-driver = webdriver.Firefox() # Supported browsers include Chrome, Firefox, and Safari
+# driver = webdriver.Chrome() # Supported browsers include Chrome, Firefox, and Safari
 
 # driver.get("url") get the URL (protocol needs to be specified)
+
+dataFrame = pd.read_csv("Consulting Test - Question Details.csv") # Include the name of each csv file here
+
+specifiedFrame = dataFrame[["Q #", "Q Text", "Answer", "Answer Match"]]
+
+print(specifiedFrame)
