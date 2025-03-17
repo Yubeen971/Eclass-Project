2fbd864524e6f6affc8bfd41dffd2eaf8b8ea835
AmcL <mcleodamare@gmail.com>
2025-03-17 14:50:51 -0400

    UI Test

---
+++ ui.py
@@ -0,0 +1,42 @@
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+wasClicked = False
+class FirstWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = "CSV Path Chosen"
+        self.success = "Quiz Transferred Successfully!"
+        self.bye = "Please Choose a CSV Path"
+        self.button = QtWidgets.QPushButton("Choose the path to your csv file")
+        self.button2 = QtWidgets.QPushButton("Move Quiz Data")
+        self.text = QtWidgets.QLabel("Welcome to the Quiz Relocator", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.button2)
+
+        self.button.clicked.connect(self.onClicked)
+        self.button2.clicked.connect(self.onMoveClick)
+    
+    def onClicked(self):
+        self.text.setText(self.hello)
+        wasClicked = True
+        return wasClicked
+    
+    def onMoveClick(self):
+        if (wasClicked):
+            self.text.setText(self.success)
+
+
+if (__name__ == "__main__"):
+    app = QtWidgets.QApplication([])
+
+    widget = FirstWidget()
+    widget.resize(400, 300)
+    widget.show()
+
+    sys.exit(app.exec())
